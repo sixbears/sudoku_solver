48c0ae8c38a7314df66fdccd237a57ad5211a5d8
Jules VENDEE <jules.vendee@ynov.com>
2022-05-28 17:49:13 +0200

    intermediaire sudoku

--- Parameter.py
+++ Parameter.py
@@ -1,7 +1,7 @@
 class Parameter:
-    nbreIndividus = 10
+    nbreIndividus = 1000
     maxFitness = 81
-    generationMaxNb = 10
+    generationMaxNb = 10000
     mutationRate = 0.9
     crossOverRate = 0.8
     choosingGenomeRate = 0.5
